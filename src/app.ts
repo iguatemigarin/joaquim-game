b3f492d6434122d8da5e1e9988509ccf3df4b554
Ubuntu <ubuntu@ip-10-240-153-39.us-west-2.compute.internal>
2024-06-15 20:34:37 +0000

    Fix Stage class instantiation without arguments

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { loop } from './render/loop'
 import { Stage } from './stage'
 import { renderTree } from './render/render-tree'
 
-const stage = new Stage('main stage')
+const stage = new Stage()
 
 const fps = new FPS('fps')
 
